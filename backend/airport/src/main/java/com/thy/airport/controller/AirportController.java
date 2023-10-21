d805c122031733f5be4eb02afc3594ca181b356e
dilan.yilmaz <dilan.yilmaz@vbt.com.tr>
2023-10-22 00:21:27 +0300

    ++ Authentication and authorization were added using Keycloak.

--- backend/airport/src/main/java/com/thy/airport/controller/AirportController.java
+++ backend/airport/src/main/java/com/thy/airport/controller/AirportController.java
@@ -6,6 +6,7 @@ import com.thy.airport.dto.core.ResponsePayload;
 import com.thy.airport.repository.AirportRepository;
 import com.thy.airport.service.AirportService;
 import java.util.List;
+import javax.annotation.security.RolesAllowed;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,12 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AirportController {
   private final AirportService airportService;
   private final AirportRepository airportRepository;
+
   @GetMapping(value = "/listAirports")
   public ResponsePayload<List<AirportDTO>> listAirports(){
     return airportService.list();
   }
 
+  //this method can be accessed by user whose role is admin
   @GetMapping(value = "/listAirportsByCountry")
+  @RolesAllowed("admin")
   public ResponsePayload<List<Airports>> listAirportsByCountry(@RequestParam(value = "id") int id){
     return airportService.listA(id);
   }
